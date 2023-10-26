ffee0dd210f0d579f560144873bf8250d49f652a
yul-nk97 <66767368+yul-nk97@users.noreply.github.com>
2023-10-26 16:46:24 +0200

    Add files via upload

--- setup.py
+++ setup.py
@@ -5,23 +5,21 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setup(
-    name="chem_eng_ml",
-    version="0.0.1",
-    author="Matthias Schniewind",
-    author_email="matthias.schniewind@kit.edu",
+    name="chem_eng_ai",
+    version="0.0.2",
+    author="Yuri Koide, Arjun J. Kaithakkal, Matthias Schniewind, Bradley P. Ladewig, Alexander Stroh, Pascal Friederich",
+    author_email="yuri.koide@kit.edu",
     description="Predicting flow properties from channel geometries for Chemical Engineering.",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="",
-    install_requires=["numpy",
-                      "pandas",
-                      "mlflow",
-                      "scikit-learn",
-                      "h5py",
-                      "tables",
-                      "tensorflow>=2.3.0"],
+    install_requires=["tensorflow==2.5.0",
+                      "scikit-learn==0.24.2",
+                      "pandas==1.3.0",
+                      "pyyaml==5.4.1"],
     packages=find_packages(),
     include_package_data=True,
+    package_data={"keras_addons": ["*.json", "*.yaml"]},
     classifiers=[
         "Programming Language :: Python :: 3",
         "Operating System :: OS Independent",
@@ -29,5 +27,5 @@ setup(
         "Topic :: Scientific/Engineering :: Information Analysis",
         "Topic :: Software Development :: Libraries :: Python Modules"
     ],
-    keywords=["chemical", "engineering", "machine", "learning", "deep", "networks", "neural", "keras"]
+    keywords=["Chemical engineering", "Machine learning", "Numerical simulation", "Convolutional neural networks", "Fluid dynamics"]
 )
